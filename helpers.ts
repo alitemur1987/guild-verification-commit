ce8df597c194d3372c219551763f5425b16635b9
alitemur1987 <139598651+alitemur1987@users.noreply.github.com>
2026-03-06 15:16:37 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:16:29.583Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T12:16:37.059Z
+Commit: Resolve null pointer exception in user service
