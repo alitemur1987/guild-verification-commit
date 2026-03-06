bdd2c87ef22b8f2639361a66f8935d3fa97e31a1
alitemur1987 <139598651+alitemur1987@users.noreply.github.com>
2026-03-06 15:17:42 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:17:38.292Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T12:17:42.317Z
+Commit: Fix CSS styling bug on mobile devices
