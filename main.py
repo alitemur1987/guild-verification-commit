8312df642c7b236f8d520c44554e065222c45ca5
alitemur1987 <139598651+alitemur1987@users.noreply.github.com>
2026-03-06 15:18:30 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:18:12.879Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T12:18:30.468Z
+Commit: Resolve null pointer exception in user service
